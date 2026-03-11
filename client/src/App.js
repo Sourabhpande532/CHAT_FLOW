24a5a7d62ed4a622eb747c9b0448305848143cfd
sourabhpande532 <sourabhpande43@gmail.com>
2026-03-11 15:14:06 +0530

    Setup: login & register done!

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Chat } from "./components/Chat";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div>Adding chat component</div>
+    <div className='app'>
+      <h1>Chat app</h1>
+      {!user ? (
+        <div className='container mt-5 text-center'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <Register setUser={setUser} />
+            </div>
+            <div className='col-md-6'>
+              <Login setUser={setUser} />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Chat />
+      )}
+    </div>
   );
 }
 
---
+++ client/src/components/Chat.js
@@ -0,0 +1,8 @@
+const Chat = () => {
+  return (
+    <div>
+      <h2>Chat App</h2>
+    </div>
+  );
+};
+export { Chat };
